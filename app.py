b62fe1f1c3c0901b5b572532fcc563c073c4755d
peg24 <pegdesember@hotmail.com>
2021-04-20 22:04:39 +0200

    Updated app.py

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-#print("/___|")
-#print("   /|")
-#print("  / |")
-#print(" /  |")
-
-print("@@@@    @     ")
-print("@  @   @@@")
-print("@  @   @ @")
-print("@  @   @@@")
-print("@@@@ Hei  @ @")
+print("/___|")
+print("   /|")
+print("  / |")
+print(" /  |")
 
 
 
+character_name = "John"
+character_age = "35"
+print("There once was a man name " + character_name + ", ")
+print("His age was " + character_age + ". ")
 
-#character_name = "John"
-#character_age = "35"
-#print("There once was a man name " + character_name + ", ")
-#print("His age was " + character_age + ". ")
-
-#character_age = 50
-#print("There once was a man name " + character_name + ", ")
-#print("His age was " + character_age + ". ")
+character_age = 50
+print("There once was a man name " + character_name + ", ")
+print("His age was " + character_age + ". ")
